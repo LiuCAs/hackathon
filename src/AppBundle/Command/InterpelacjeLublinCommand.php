52b6957f5f7a7011d7525ececbc8a16c2f21357f
Jędrzej Witaszak <jedrzej.witaszak@kinguin.net>
2016-09-25 08:53:57 +0200

    try-catch and config endpoint

---
+++ src/AppBundle/Command/InterpelacjeLublinCommand.php
@@ -0,0 +1,150 @@
+<?php
+// src/AppBundle/Command/InterpelacjeCommand.php
+namespace AppBundle\Command;
+
+use AppBundle\Entity\Point;
+use stdClass;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+
+class InterpelacjeLublinCommand extends ContainerAwareCommand
+{
+    const ITEM_PER_PAGE = 100;
+
+    const CITY_CODE = "0663";
+
+    const GOOGLE_API_KEY = "AIzaSyAVmwCNz1smHBx6C1I1h-lXzs6U2HdHQUo";
+
+    const DATA_ADDRESS = 'http://bip.lublin.eu/api-json/in_rm_vii/';
+
+    private $invalidWords = [];
+
+    private $allowedWords = [
+        'ul.', 'ulic', 'ulica', 'ulicy'
+    ];
+
+    private $categoryId;
+
+    protected function configure()
+    {
+        $this
+            ->setName('app:interpelacjeLublin')
+            ->addArgument('category_id', InputArgument::REQUIRED, 'ID Kategori')
+            ->setDescription('Parsowanie interpelacji');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->categoryId = $input->getArgument('category_id');
+        $this->prepareParser();
+    }
+
+    private function prepareParser()
+    {
+        $json = file_get_contents(self::DATA_ADDRESS);
+
+        $jsonDecoded = json_decode($json);
+        $returnArray = [];
+        foreach ($jsonDecoded->items as $item) {
+            $PointModel = new Point();
+            try {
+                $PointModel->setCategory($this->categoryId);
+                $PointModel->setCity(self::CITY_CODE);
+                $PointModel->setSubject($item->sprawa);
+                $PointModel->setDate($item->data_wp);
+                $PointModel->setInternalId($item->id);
+                $attachmentArray = [
+                    'urls' => [
+                        $item->link
+                    ]
+                ];
+                $streetQuery = $this->prepareQuery($item->sprawa);
+                $street = $this->getStreet($streetQuery);
+                $PointModel->setAttachments(json_encode($attachmentArray));
+
+                if (!empty($street)) {
+                    $PointModel->setStreet($street);
+                    $geo = $this->getLatLong([$street]);
+                    $PointModel->setLat($geo->lat);
+                    $PointModel->setLng($geo->lng);
+                }
+                $em = $this->getContainer()->get('doctrine')->getManager('default');
+                $em->persist($PointModel);
+                $em->flush();
+            } catch (Exception $e) {
+                return 0;
+            }
+
+        }
+        return $returnArray;
+    }
+
+    private function prepareQuery($subject)
+    {
+        //basic clean
+        $subject = trim($subject);
+        $subject = str_replace('-', " ", $subject);
+        $subject = stripslashes($subject);
+        $explodedSubject = explode(" ", $subject);
+
+        //looking for allowed words, then return max 4 words after that
+        foreach ($explodedSubject as $key => $subjectWord) {
+            if (in_array($subjectWord, $this->allowedWords)) {
+                $subjectCount = count($explodedSubject);
+                if ($key < $subjectCount) {
+                    $maxIterations = $subjectCount - $key;
+                    $returnArray = [];
+                    for ($i = 1; $i < $maxIterations; $i++) {
+                        $returnArray[] = $explodedSubject[$key + $i];
+                        if ($i >= 4) {
+                            break;
+                        }
+                    }
+                    return $returnArray;
+                }
+            }
+        }
+
+        //remove invalid and too short
+        $test = array_filter($explodedSubject, function ($elem) {
+            return !in_array(strtolower($elem), $this->invalidWords) && strlen($elem) > 2;
+        });
+
+        return $test;
+    }
+
+    private function getStreet($array)
+    {
+//        $location = new \AppBundle\Utils\Location(1, 1);
+//        $response = $location->gdzieJestSeba(self::CITY_CODE, implode(" ", $array));
+        return isset($array[0]) ? $array[0] : [];
+//        return $array;
+    }
+
+    private function getLatLong($address)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager('default');
+
+        $entity = $em->getRepository('AppBundle:Point')->findOneBy(['street' => $address[0], 'city' => self::CITY_CODE]);
+
+        if ($entity) {
+            $stdClass = new stdClass();
+            $stdClass->lat = $entity->getLat();
+            $stdClass->lng = $entity->getLng();
+            return $stdClass;
+        }
+
+        $baseAddress = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+        $addressString = implode("+", $address);
+        $addressString .= ",+Lublin";
+        $addressString .= ",+Poland";
+        $addressString .= "&key=" . self::GOOGLE_API_KEY;
+        $url = $baseAddress . $addressString;
+        $json = json_decode(file_get_contents($url));
+        return $json->results[0]->geometry->location;
+    }
+}
