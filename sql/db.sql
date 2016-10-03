8dae7eb4d359cdfdeef8b2336c75c49deebaf8bd
Jędrzej Witaszak <jedrzej.witaszak@kinguin.net>
2016-10-03 16:47:58 +0200

    add db dump with api and categories

--- sql/db.sql
+++ sql/db.sql
@@ -29,7 +29,10 @@ CREATE TABLE `category` (
   `lat` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `lng` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `city_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `api_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_64C19C154963938` (`api_id`),
+  CONSTRAINT `FK_64C19C154963938` FOREIGN KEY (`api_id`) REFERENCES `api` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,8 +42,34 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (1,'3064','Interpelacje','52.406374','16.9251681','Poznań'),(2,'3064','Oferty pracy','52.406374','16.9251681','Poznań'),(3,'0663','Interpelacje','51.2464536','22.5684463','Lublin');
+INSERT INTO `category` VALUES (1,'3064','Interpelacje','52.406374','16.9251681','Poznań',1),(2,'3064','Oferty pracy','52.406374','16.9251681','Poznań',2),(3,'0663','Interpelacje','51.2464536','22.5684463','Lublin',3);
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `api`
+--
+
+DROP TABLE IF EXISTS `api`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `api` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_per_page` int(11) DEFAULT NULL,
+  `data_address` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `api`
+--
+
+LOCK TABLES `api` WRITE;
+/*!40000 ALTER TABLE `api` DISABLE KEYS */;
+INSERT INTO `api` VALUES (1,50,'http://bip.poznan.pl/api-json/bip/interpelacje/','Poznań - Interpelacje'),(2,20,'http://bip.poznan.pl/api-json/bip/oferty-pracy/','Poznań - Oferty pracy'),(3,100,'http://bip.lublin.eu/api-json/in_rm/','Lublin - Interpelacje');
+/*!40000 ALTER TABLE `api` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -52,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-25 14:21:53
+-- Dump completed on 2016-10-03 16:46:17
